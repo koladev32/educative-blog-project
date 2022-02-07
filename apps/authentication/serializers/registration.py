2a2d3e666b86f81eb7296f26e1eefdbaf921e2fc
koladev <onaelmangabo@gmail.com>
2022-02-07 01:00:37 +0100

    - feat: add registration serializer

--- apps/authentication/serializers/registration.py
+++ apps/authentication/serializers/registration.py
@@ -16,7 +16,8 @@ class RegisterSerializer(UserSerializer):
     class Meta:
         model = User
         # List of all the fields that can be included in a request or a response
-        fields = ['id', 'bio', 'avatar', 'email', 'username', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'first_name', 'last_name', 'name', 'email', 'is_active',
+                  'created', 'updated']
 
     def create(self, validated_data):
         # Use the `create_user` method we wrote earlier for the UserManager to create a new user.
