dff03d1a7b76afb27a43ac5f6c5c0e6d6cff8580
koladev <onaelmangabo@gmail.com>
2022-02-27 16:00:16 +0100

    - feat: add registration viewset

--- apps/authentication/viewsets/registration.py
+++ apps/authentication/viewsets/registration.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.authentication.serializers import RegistrationSerializer
 
@@ -12,3 +14,20 @@ class RegistrationViewSet(viewsets.ViewSet):
     http_methods_names = ['post']
     permissions_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+        return Response({
+            "user": serializer.data,
+            "refresh": tokens["refresh"],
+            "token": tokens["access"]
+        }, status=status.HTTP_201_CREATED)
