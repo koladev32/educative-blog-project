6e41a5e58614f829dcb6db179e029ff30fdeb232
koladev <onaelmangabo@gmail.com>
2022-03-08 02:33:26 +0100

    - feat: add comment model

--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.abstract.models import AbstractManager, AbstractModel
+
+
+class CommentManager(AbstractManager):
+    pass
+
+class Comment(AbstractModel):
+    author = models.ForeignKey('apps_user.User', on_delete=models.CASCADE)
+    article = models.ForeignKey('apps_article.Article', on_delete=models.CASCADE)
+    body = models.TextField()
+
+    objects = CommentManager()
