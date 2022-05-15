dc5f5eed82430ce509471009c7406c4b8b53959f
koladev <onaelmangabo@gmail.com>
2022-05-15 17:34:11 +0100

    ✅ add user tests

---
+++ apps/user/tests/test_models.py
@@ -0,0 +1,19 @@
+import pytest
+
+from apps.user.models import User
+
+
+@pytest.djanog_db
+def test_user_creation(db):
+    user = User.objects.create_user(
+        username='test',
+        first_name="John",
+        last_name="Doe",
+        password="12345",
+        email="johndoe@yopmail"
+    )
+
+    assert user.username == 'test'
+    assert user.first_name == 'John'
+    assert user.last_name == 'Doe'
+    assert user.email == 'johndoe@yopmail'
